75ee4b046a9fd311a2b481e1b65d9f5d26dd6642
Ryan Williams <rewilli95@gmail.com>
2019-04-05 22:48:25 -0700

    Got accumulator almost working it just wont update on timer trigger.

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _AppState extends State<MyApp> {
         bloc: _themeBloc,
         builder: (_, ThemeData theme) {
           return MaterialApp(
-            title: 'Flutter Demo',
+            title: 'Flutter TapApp',
             home: CounterPage(),
             theme: theme,
           );
@@ -59,12 +59,12 @@ class _AppState extends State<MyApp> {
   }
 }
 
-bool _upgradeBuyButtonEnable = false;
+
 
 class UpgradeCard extends StatelessWidget {
 
   //final int index;
-  VoidCallback onPressed;
+  final VoidCallback onPressed;
   final String upgradeName;
   final String upgradeCost;
   final IconData upgradeIcon;
@@ -73,12 +73,10 @@ class UpgradeCard extends StatelessWidget {
   UpgradeCard(this.upgradeName, this.upgradeCost, this.onPressed, {this.upgradeIcon = Icons.add_circle});
 
 
+
   @override
   Widget build(BuildContext context) {
-    if(!_upgradeBuyButtonEnable)
-    {
-      onPressed = null;
-    }
+    
     return Card(
       child: Column(
         children: [
@@ -101,14 +99,12 @@ class UpgradeCard extends StatelessWidget {
 
 class CounterPage extends StatelessWidget {
   
-  //final List<int> _items = List.generate(100, (int index) => index);
-
   @override
   Widget build(BuildContext context) {
     final CounterBloc _counterBloc = BlocProvider.of<CounterBloc>(context);
     //final ThemeBloc _themeBloc = BlocProvider.of<ThemeBloc>(context);
     return Scaffold(
-      appBar: AppBar(title: Text('Counter')),
+      appBar: AppBar(title: Text('Flutter TapApp')),
       body: BlocBuilder<CounterEvent, CounterState>(
         bloc: _counterBloc,
         builder: (BuildContext context, CounterState state) {
@@ -121,12 +117,16 @@ class CounterPage extends StatelessWidget {
                 
               ),
               Expanded(child: UpgradeCard(
-                'Better Clicks ${CounterBloc.randomValue}',
-                state.purchaseCost.toString(),
+                'Better Clicks ${CounterBloc.upgradeRandomValue}',
+                state.upgradePurchaseCost.toString(), _counterBloc.upgradeBuyButtonEnable == true ? 
                 () {
-                  _counterBloc.dispatch(Purchase());
-                },
+                  _counterBloc.dispatch(UpgradePurchase());
+                } : null,
               )),
+              Expanded(child: UpgradeCard('+${CounterBloc.accumRandomValue} Accumulator', state.accumPurchaseCost.toString(), _counterBloc.accumulatorBuyButtonEnable == true ? 
+                () {
+                  _counterBloc.dispatch(AccumPurchase());
+                } : null),),
               SizedBox(
                   width:double.infinity,
                   child: RaisedButton(
@@ -146,25 +146,32 @@ class CounterPage extends StatelessWidget {
 
 abstract class CounterEvent {  }
 class IncrementCounter extends CounterEvent{ }
-class Purchase extends CounterEvent{}
+class UpgradePurchase extends CounterEvent{}
+class AccumIncrementCounter extends CounterEvent{}
+class AccumPurchase extends CounterEvent{}
+
+
 
 const int _kMinRandomValue = 1;
 const int _kMaxRandomValue = 10; 
 
 class CounterState {
-  final int purchaseCost;
+  final int upgradePurchaseCost;
+  final int accumPurchaseCost;
   final int bank;
   final String upgradeName;
 
-  CounterState(this.purchaseCost, this.bank, {this.upgradeName});
+  CounterState(this.upgradePurchaseCost, this.accumPurchaseCost, this.bank, {this.upgradeName});
 
   CounterState copyWith({
-    int purchaseCost,
+    int upgradePurchaseCost,
+    int accumPurchaseCost,
     int bank,
     String upgradeName
   }) {
     return CounterState(
-      purchaseCost ?? this.purchaseCost,
+      upgradePurchaseCost ?? this.upgradePurchaseCost,
+      accumPurchaseCost ?? this.accumPurchaseCost,
       bank ?? this.bank
     );
   }
@@ -172,58 +179,119 @@ class CounterState {
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
 
-  CounterBloc(){
-    randomValue = _kMinRandomValue + rnd.nextInt(_kMaxRandomValue - _kMinRandomValue);
-  }
-
-  @override
-  CounterState get initialState => CounterState(10, 0);
-
+  bool _upgradeBuyButtonEnable = false;
+  bool get upgradeBuyButtonEnable => _upgradeBuyButtonEnable;
+  int incrementCount = 1;
+  int accumIncrement = 0;
   static Random rnd = Random();
   final int min = 1;
   final int max = 10;
-  
-  static int randomValue;
+  static int upgradeRandomValue = _kMinRandomValue + rnd.nextInt(_kMaxRandomValue - _kMinRandomValue);
+  static int accumRandomValue = _kMinRandomValue + rnd.nextInt(_kMaxRandomValue - _kMinRandomValue);
+
   final int startingCost = 10;
   final int percentageCostIncrease = 15;
 
-  int incrementCount = 1;
+  Timer accumTimer;
+  
+
+  @override
+  CounterState get initialState => CounterState(10, 100, 0);
+
+  bool _accumulatorBuyButtonEnable = false;
+  bool get accumulatorBuyButtonEnable => _accumulatorBuyButtonEnable;
+
+  
 
   @override
   Stream<CounterState> mapEventToState(CounterEvent event) async * {
     if(event is IncrementCounter) {
 
       // purchaseButtonEnabled = purchaseCost <= bank;
-      if((currentState.bank+incrementCount) >= currentState.purchaseCost)
+      if((currentState.bank+incrementCount) >= currentState.upgradePurchaseCost)
       {
         _upgradeBuyButtonEnable = true;
       }
 
+      if((currentState.bank + incrementCount) >= currentState.accumPurchaseCost)
+      {
+        _accumulatorBuyButtonEnable = true;
+      }
+
       yield CounterState(
-        currentState.purchaseCost,
+        currentState.upgradePurchaseCost,
+        currentState.accumPurchaseCost,
         currentState.bank + incrementCount
       );
     }
 
-    if(event is Purchase) {
+    if(event is UpgradePurchase) {
 
 
       // Deal with bank amount changes
-      incrementCount += randomValue;
-      randomValue = _kMinRandomValue + rnd.nextInt(_kMaxRandomValue - _kMinRandomValue);
-      final newPurchaseCost = currentState.purchaseCost + incrementCount;
-      final newBank = currentState.bank - currentState.purchaseCost;
+      incrementCount += upgradeRandomValue;
+      upgradeRandomValue = _kMinRandomValue + rnd.nextInt(_kMaxRandomValue - _kMinRandomValue);
+      final newPurchaseCost = currentState.upgradePurchaseCost + incrementCount;
+      final newBank = currentState.bank - currentState.upgradePurchaseCost;
 
       //Update the button state
       if(newBank < newPurchaseCost)
       {
         _upgradeBuyButtonEnable = false;
       }
-
+      if(newBank < currentState.accumPurchaseCost)
+      {
+        _accumulatorBuyButtonEnable = false;
+      }
       yield CounterState(
         newPurchaseCost,
+        currentState.accumPurchaseCost,
         newBank
       );
+    }
+
+    if(event is AccumPurchase)
+    {
+      accumIncrement += accumRandomValue;
+
+      if(accumIncrement == accumRandomValue)
+      {
+        accumTimer = Timer.periodic(Duration(seconds: 1), (Timer t)=>(){this.dispatch(AccumIncrementCounter());});
+      }
+      
+      if((currentState.bank-currentState.accumPurchaseCost) < currentState.upgradePurchaseCost)
+      {
+          _upgradeBuyButtonEnable = false;
+      }
+      if((currentState.bank-currentState.accumPurchaseCost) < (currentState.accumPurchaseCost*2))
+      {
+          _accumulatorBuyButtonEnable = false;
+      }
+
+      yield CounterState(
+        currentState.upgradePurchaseCost, 
+        (currentState.accumPurchaseCost*2), 
+        (currentState.bank-currentState.accumPurchaseCost));
+    }
+    if(event is AccumIncrementCounter)
+    {
+        if((currentState.bank + accumIncrement) >= currentState.upgradePurchaseCost)
+        {
+          _upgradeBuyButtonEnable = true;
+        }
+
+        if((currentState.bank + accumIncrement) >= currentState.accumPurchaseCost)
+        {
+         _accumulatorBuyButtonEnable = true;
+        }
+
+        accumRandomValue = _kMinRandomValue + rnd.nextInt(_kMaxRandomValue - _kMinRandomValue);
+        
+        yield CounterState(
+          currentState.upgradePurchaseCost,
+          currentState.accumPurchaseCost,
+          currentState.bank + accumIncrement 
+        );
     }
   }
 }
